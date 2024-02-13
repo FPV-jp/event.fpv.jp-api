5cb18e5b88710bfdd805f630e0f4e17b2243fd9d
tantaka <tomokazu.tantaka@gmail.com>
2024-02-13 13:08:47 +0900

    update

--- src/DI/PermissionMiddleware.php
+++ src/DI/PermissionMiddleware.php
@@ -5,7 +5,7 @@ namespace FpvJp\DI;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Slim\Routing\RouteContext;
-use Auth0\SDK\Exception\InvalidTokenException;
+use Auth0\SDK\Exception\Auth0Exception;
 use Auth0\SDK\Configuration\SdkConfiguration;
 use Auth0\SDK\Token;
 use Nyholm\Psr7\Response;
@@ -19,18 +19,13 @@ class PermissionMiddleware
         if ($bearer == null) {
             return new Response(403, ['Content-Type' => 'application/json'], json_encode(['error' => 'Authorization token required']));
         }
-
+        error_log($bearer);
         try {
-            // Token::TYPE_ACCESS_TOKEN
-            // Token::TYPE_ID_TOKEN
-            $this->validateToken($bearer, Token::TYPE_ACCESS_TOKEN);
-        } catch (InvalidTokenException $e) {
+            // $this->validateToken($bearer, Token::TYPE_ACCESS_TOKEN);
+            $this->validateToken($bearer, Token::TYPE_ID_TOKEN);
+        } catch (Auth0Exception $e) {
             return new Response(403, ['Content-Type' => 'application/json'], json_encode(['error' => 'Auth0 Token Validation Error: ' . $e->getMessage()]));
         }
-
-        // error_log('nickname: ' . $idToken['nickname']);
-        // error_log('email: ' . $idToken['email']);
-        // error_log('sub: ' . $idToken['sub']);
 
         $routeContext = RouteContext::fromRequest($request);
         $route = $routeContext->getRoute();
@@ -59,8 +54,6 @@ class PermissionMiddleware
         $auth0Configuration = [
             'domain' => $_ENV['AUTH0_DOMAIN'],
             'clientId' => $_ENV['AUTH0_CLIENT_ID'],
-            'clientSecret' => $_ENV['AUTH0_CLIENT_SECRET'],
-            'tokenAlgorithm' => $_ENV['AUTH0_TOKEN_ALGORITHM'],
             'cookieSecret' => bin2hex(random_bytes(32)),
         ];
         $config = new SdkConfiguration($auth0Configuration);
